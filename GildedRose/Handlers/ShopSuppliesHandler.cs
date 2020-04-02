fa2c08bafc59b3bf20614736a87b89e857a2f817
Andrius <Sumskas.An@gmail.com>
2020-04-02 20:59:53 +0300

    Supplier handler Commit
    Created special supplier handler class to provide data for retrieving supplies

---
+++ GildedRose/Handlers/ShopSuppliesHandler.cs
@@ -0,0 +1,41 @@
+﻿using GildedRose.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRose.Handlers
+{
+    public class ShopSuppliesHandler
+    {
+        public ShopSuppliesHandler()
+        {
+        }
+
+        /// <summary>
+        /// Method used to retrieve all the supplies from different markets.
+        /// Note: This method can be later used to merge different ways of receiving supplies (Files, DB, Hardcoded)
+        /// </summary>
+        public List<Item> GetSupplies()
+        {
+            List<Item> supplies = new List<Item>();
+            supplies.AddRange(GetLocalMarketSupplies());
+            return supplies;
+        }
+
+        private List<Item> GetLocalMarketSupplies()
+        {
+            List<Item> result = new List<Item>();
+            result.Add(new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 });
+            result.Add(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 });
+            result.Add(new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 });
+            result.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 });
+            result.Add(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 });
+            result.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20 });
+            result.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 });
+            result.Add(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49 });
+            result.Add(new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 });
+
+            return result;
+        }
+    }
+}
--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -1,4 +1,5 @@
-﻿using GildedRose.Objects;
+﻿using GildedRose.Handlers;
+using GildedRose.Objects;
 using System;
 using System.Collections.Generic;
 
@@ -10,44 +11,19 @@ namespace GildedRose
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
+            ShopSuppliesHandler suppliesHandler = new ShopSuppliesHandler();
 
-            var app = new GildedRose(Items);
+            List<Item> supplies = suppliesHandler.GetSupplies();
 
+            GildedRose app = new GildedRose(supplies);
 
             for (var i = 0; i < 31; i++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                Console.WriteLine("-------- Day " + i + " --------");
+                Console.WriteLine("Name, SellIn, Quality");
+                foreach (Item item in supplies)
                 {
-                    System.Console.WriteLine(Items[j]);
+                    Console.WriteLine(item);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
